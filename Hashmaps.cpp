2e6e915bd89566e0b8b4b485adc5ea66404ab555
aryan-212 <allalonearyan@gmail.com>
2024-08-07 23:54:09 +0530

    Hashamps

---
+++ Hashmaps.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int hashquery(int query, int arr[], int size) {
+  int hsh[101] = {0}; // Assuming the values in arr are between 0 and 100
+  for (int i = 0; i < size; i++) {
+    hsh[arr[i]] += 1;
+  }
+  return hsh[query];
+}
+
+int main() {
+  int size;
+  cin >> size;
+  int arr[size];
+  for (int i = 0; i < size; i++) {
+    cin >> arr[i];
+  }
+  int q;
+  cin >> q;
+  while (q--) {
+    int number;
+    cin >> number;
+    cout << number << " has appeared " << hashquery(number, arr, size)
+         << " times\n";
+  }
+  return 0;
+}
