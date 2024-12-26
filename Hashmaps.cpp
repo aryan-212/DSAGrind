fe9160f8dedf8548a6fff680c72cba79fb2d25f5
aryan-212 <allalonearyan@gmail.com>
2024-12-26 19:26:11 +0530

    hashmaps optimised

--- Hashmaps.cpp
+++ Hashmaps.cpp
@@ -1,14 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int hashquery(int query, int arr[], int size) {
-  int hsh[101] = {0}; // Assuming the values in arr are between 0 and 100
-  for (int i = 0; i < size; i++) {
-    hsh[arr[i]] += 1;
-  }
-  return hsh[query];
-}
-
 int main() {
   int size;
   cin >> size;
@@ -16,13 +8,20 @@ int main() {
   for (int i = 0; i < size; i++) {
     cin >> arr[i];
   }
+
+  // Precompute the frequency table
+  int hsh[101] = {0}; // Assuming the values in arr are between 0 and 100
+  for (int i = 0; i < size; i++) {
+    hsh[arr[i]] += 1;
+  }
+
   int q;
   cin >> q;
   while (q--) {
     int number;
     cin >> number;
-    cout << number << " has appeared " << hashquery(number, arr, size)
-         << " times\n";
+    cout << number << " has appeared " << hsh[number] << " times\n";
   }
+
   return 0;
 }
